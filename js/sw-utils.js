e427c429ceed9a85a91920531d9824efc6f33f3b
yaider <yaideralejo2@gmail.com>
2026-04-06 16:30:47 -0500

    First commit

---
+++ js/sw-utils.js
@@ -0,0 +1,8 @@
+if (res.ok || res.type === 'opaque') {
+    return caches.open(dynamicCache).then(cache => {
+        cache.put(req, res.clone());
+        return res;
+    });
+} else {
+    return res;
+}
